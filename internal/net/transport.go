ddae7580c2bf89b5b4aa3fdf137801f64d33b3ef
xevrion <krbavadiya11@gmail.com>
2026-01-27 23:22:08 +0530

    feat: implement NewTransport, Start, and Stop methods for Transport struct

--- internal/net/transport.go
+++ internal/net/transport.go
@@ -5,3 +5,17 @@ import "echo-go/internal/core"
 type Transport struct {
 	manager *core.Manager
 }
+
+func NewTransport(manager *core.Manager) *Transport {
+	return &Transport{
+		manager: manager,
+	}
+}
+
+func (t *Transport) Start() error {
+	return nil
+}
+
+func (t *Transport) Stop() error {
+	return nil
+}
