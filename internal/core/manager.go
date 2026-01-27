c0a64f4155f7f7fa2f2e28e45e65f40e2be6644e
xevrion <krbavadiya11@gmail.com>
2026-01-27 22:37:14 +0530

    feat: implement Send and Receive methods in Manager for message handling

--- internal/core/manager.go
+++ internal/core/manager.go
@@ -1,5 +1,7 @@
 package core
 
+import "time"
+
 type Manager struct {
 	config Config
 	peers  map[string]Peer
@@ -11,5 +13,31 @@ func NewManager(config Config) *Manager {
 		config: config,
 		peers:  make(map[string]Peer),
 		events: make(chan Event, 100),
+	}
+}
+
+func (manager *Manager) Events() chan Event {
+	return manager.events
+}
+
+func (manager *Manager) Send(text string) {
+	// Implementation for sending a message
+	msg := Message{
+		Text:   text,
+		Sender: manager.config.Username,
+		Time:   time.Now(),
+	}
+
+	manager.events <- Event{
+		Type:    "message",
+		Payload: msg,
+	}
+
+}
+
+func (manager *Manager) Receive(msg Message) {
+	manager.events <- Event{
+		Type:    "message",
+		Payload: msg,
 	}
 }
